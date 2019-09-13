5af0b9e2a353d4e07f9a181929a7d68ff2a2872a
NikolaRoev <47421460+NikolaRoev@users.noreply.github.com>
2019-09-13 13:02:48 +0300

    Added sfml audio.

--- src/EntryPoint.cpp
+++ src/EntryPoint.cpp
@@ -4,10 +4,13 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/System.hpp>
 #include <SFML/Window.hpp>
+#include <SFML/Audio.hpp>
 
 
 int main() {
 	sf::RenderWindow window;
+
+	sf::Sound sound;
 
 	sf::VideoMode mode = { 1024, 576, 32 };
 	window.create(mode, "eNement", sf::Style::Default);
